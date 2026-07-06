46a69ad8b1f0bd76970bc69bbebd1e6c85e2f6b4
Priscillah <priscillahnjaii@gmail.com>
2026-07-06 12:56:20 +0300

    Add server entry point with dotenv config

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,6 @@
-
-const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Test Route
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Welcome to the PinkPulse API Server!",
-  });
-});
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
