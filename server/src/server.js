7e1d60ee22bb890c9e9464ab915704ee7f0494ea
Priscillah <priscillahnjaii@gmail.com>
2026-07-06 12:12:49 +0300

    Add health-check route and start server

--- server/src/server.js
+++ server/src/server.js
@@ -9,3 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Test Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to the PinkPulse API Server!",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
