de87bc2576e565664a3357838eb4f69cf3f92517
Priscillah <priscillahnjaii@gmail.com>
2026-07-07 16:13:06 +0300

    Ammend routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const categoryRoutes = require("./routes/category.routes");
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/categories", categoryRoutes);
 
 // Test route
 app.get("/", (req, res) => {
