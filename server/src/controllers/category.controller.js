9daf759b245aa6d451aefa1e6ef95e226e85aa18
Priscillah <priscillahnjaii@gmail.com>
2026-07-09 18:07:51 +0300

    Add GET single category

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -41,7 +41,35 @@ const createCategory = async (req, res) => {
   }
 };
 
+// GET single category
+const getCategoryById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await prisma.category.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!category) {
+      return res.status(404).json({
+        message: "Category not found.",
+      });
+    }
+
+    res.status(200).json(category);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to fetch category.",
+    });
+  }
+};
+
 module.exports = {
   getCategories,
+  getCategoryById,
   createCategory,
 };
