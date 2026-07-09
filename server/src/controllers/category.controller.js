0fd70b0ac57827ca7cc2f6c36ed0bded72bd47ca
Priscillah <priscillahnjaii@gmail.com>
2026-07-09 18:36:24 +0300

    Adds DELETE category

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -23,3 +23,38 @@ const updateCategory = async (req, res) => {
     });
   }
 };
+
+// DELETE category
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await prisma.category.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!category) {
+      return res.status(404).json({
+        message: "Category not found.",
+      });
+    }
+
+    await prisma.category.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).json({
+      message: "Category deleted successfully.",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to delete category.",
+    });
+  }
+};
