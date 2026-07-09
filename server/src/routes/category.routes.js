8be9a7cec29251a5d1ec80e4d7d6d19660a84112
Priscillah <priscillahnjaii@gmail.com>
2026-07-09 18:19:24 +0300

    Implement complete category CRUD operations

--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -4,11 +4,23 @@ const router = express.Router();
 
 const {
   getCategories,
+  getCategoryById,
   createCategory,
+  updateCategory,
+  deleteCategory,
 } = require("../controllers/category.controller");
 
+// Read
 router.get("/", getCategories);
+router.get("/:id", getCategoryById);
 
+// Create
 router.post("/", createCategory);
+
+// Update
+router.put("/:id", updateCategory);
+
+// Delete
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
