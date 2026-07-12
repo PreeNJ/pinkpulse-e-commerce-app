7553601cc5bee523f85e1a2cb0947b4840cd4c15
Priscillah <priscillahnjaii@gmail.com>
2026-07-12 18:57:46 +0300

    Ammendments on routes

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -3,9 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const {
+  getProducts,
   createProduct,
 } = require("../controllers/product.controller");
 
+// GET all products
+router.get("/", getProducts);
+
+// CREATE product
 router.post("/", createProduct);
 
 module.exports = router;
