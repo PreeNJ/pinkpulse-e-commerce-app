3310dfa26cca68a6480020970fdaaa0d7c000636
Priscillah <priscillahnjaii@gmail.com>
2026-07-13 23:55:51 +0300

    Generate JWT on registration and handle errors

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,23 +1,28 @@
 const prisma = require("../config/prisma");
 const bcrypt = require("bcrypt");
 const generateToken = require("../utils/generateToken");
+
 // REGISTER USER
 const registerUser = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
+
     // Check if user already exists
     const existingUser = await prisma.user.findUnique({
       where: {
         email,
       },
     });
+
     if (existingUser) {
       return res.status(400).json({
         message: "User already exists.",
       });
     }
+
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
+
     // Create user
     const user = await prisma.user.create({
       data: {
@@ -27,3 +32,26 @@ const registerUser = async (req, res) => {
         password: hashedPassword,
       },
     });
+
+    // Generate token
+    const token = generateToken(user.id);
+
+    res.status(201).json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to register user.",
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
