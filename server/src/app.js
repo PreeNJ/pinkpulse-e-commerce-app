1b829ab9841ea393778e946ecb5b3969ddcef479
Priscillah <priscillahnjaii@gmail.com>
2026-07-13 23:34:48 +0300

    Set up authentication module

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 
 const categoryRoutes = require("./routes/category.routes");
 const productRoutes = require("./routes/product.routes");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
