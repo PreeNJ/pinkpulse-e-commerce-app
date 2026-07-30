0fb801558981b50737108199f4d6a23c29869d82
Priscillah <priscillahnjaii@gmail.com>
2026-07-30 21:54:34 +0300

    Add product image upload endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
