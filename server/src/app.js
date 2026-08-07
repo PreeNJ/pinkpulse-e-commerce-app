f901c1c79a076328dc4d6bb56ef972f5a8abc8c7
Priscillah <priscillahnjaii@gmail.com>
2026-08-07 17:45:58 +0300

    Add order import routes

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const categoryRoutes = require("./routes/category.routes");
 const productRoutes = require("./routes/product.routes");
+const orderRoutes = require("./routes/order.routes");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({
