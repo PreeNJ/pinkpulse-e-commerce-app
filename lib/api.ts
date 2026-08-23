5702faf2155172b20b784ea7e49130bf5be5dc38
Priscillah <priscillahnjaii@gmail.com>
2026-08-23 19:09:53 +0300

    Connecting pp/page.tsx directly to my backend

---
+++ lib/api.ts
@@ -0,0 +1,13 @@
+const API_URL = "http://localhost:5000/api";
+
+export async function getProducts() {
+  const res = await fetch(`${API_URL}/products`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return res.json();
+}
